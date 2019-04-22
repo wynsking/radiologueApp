bc832b87dcbe22db176bb77dda5ffd9fc39c39d6
Wyna Kanga <wyna.kanga@gmail.com>
2019-04-22 11:41:36 +0200

    Initial commit

---
+++ src/main/java/com/radiologueapp/radiologue/RadiologueApplication.java
@@ -0,0 +1,13 @@
+package com.radiologueapp.radiologue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiologueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiologueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radiologueapp/radiologue/RadiologueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.radiologueapp.radiologue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RadiologueApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
